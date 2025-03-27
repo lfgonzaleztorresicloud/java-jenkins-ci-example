d12c6ffdce8712a62a32bb7569a057afb4e26e9e
Luis Felipe Gonzalez Torres <lfgonzaleztorres@icloud.com>
2025-03-27 16:20:39 -0500

    Cambio de mensaje

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -2,6 +2,6 @@ package com.ejemplo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo Gigante");
     }
  }
